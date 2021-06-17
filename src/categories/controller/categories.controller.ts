08dc05922b340f0a0a6818f8eca9c53598d5bf8d
Nujood <shawar.nujood@gmail.com>
2021-06-17 16:18:59 +0300

    Blog #9: create category services & controlling CRUD ops

---
+++ src/categories/controller/categories.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Request,
+} from "@nestjs/common";
+import { DeleteResult, UpdateResult } from "typeorm";
+import { CreateCategoryDto } from "../model/category.dto";
+import { CategoryInterface } from "../model/category.interface";
+import { CategoriesService } from "../service/categories.service";
+
+@Controller("categories")
+export class CategoriesController {
+  constructor(private readonly categoryService: CategoriesService) {}
+
+  @Get()
+  async findAll(): Promise<CategoryInterface[]> {
+    return await this.categoryService.findAll();
+  }
+
+  @Get(":id")
+  async findOne(@Param("id") id: string): Promise<CategoryInterface> {
+    return await this.categoryService.findOne(id);
+  }
+
+  @Post()
+  async create(
+    @Body() categoryEntry: CreateCategoryDto,
+    @Request() req
+  ): Promise<CategoryInterface> {
+    return this.categoryService.create(categoryEntry);
+  }
+
+  @Put(":id")
+  async updateBlog(
+    @Param("id") id: string,
+    @Body() categoryEntry: CreateCategoryDto
+  ): Promise<UpdateResult> {
+    return this.categoryService.update(id, categoryEntry);
+  }
+
+  @Delete(":id")
+  async deleteCategory(@Param("id") id: string): Promise<DeleteResult> {
+    return this.categoryService.delete(id);
+  }
+}
