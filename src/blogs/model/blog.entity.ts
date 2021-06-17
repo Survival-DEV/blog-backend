3b00bf55e17fe9e73aed05956deb5d033ee1e011
Nujood <shawar.nujood@gmail.com>
2021-06-17 13:01:26 +0300

    Blog #6: Create Blog schema

---
+++ src/blogs/model/blog.entity.ts
@@ -0,0 +1,53 @@
+import { type } from "os";
+import { BeforeUpdate, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('blog')
+export class BlogEntity {
+  @PrimaryGeneratedColumn()
+  id: string
+
+  @Column()
+  title: string
+
+  @Column()
+  metaTitle: string
+
+  @Column()
+  slug: string
+
+  @Column({default:''})
+  summary: string
+
+  @Column({type:'timestamp', default:()=>"CURRENT_TIMESTAMP"})
+  createdAt: Date
+
+  @Column({type:'timestamp', default:()=>"CURRENT_TIMESTAMP"})
+  updatedAt: Date
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedAt= new Date
+  }
+
+  @Column({ default: 0 })
+  claps: number
+  
+  @Column({ nullable: true })
+  headerImage: string  
+
+  @Column({nullable: true})
+  publishedAt: Date
+
+  @Column({ default:''})
+  content: string
+
+  @Column({nullable:true})
+  isDraft: boolean
+
+  @Column({nullable:true})
+  parentId: number
+
+  @ManyToOne(type => UserEntity, user => user.blogEntry)
+  author: UserEntity;
+
+}
