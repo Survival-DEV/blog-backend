65c68a5d6c941fcaf2130cda64a21a51c7e0bd5d
Nujood <shawar.nujood@gmail.com>
2021-06-17 16:16:53 +0300

    Blog #9:  Initate a category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from "@nestjs/common";
+import { Logger, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { CategoriesModule } from "./categories/categories.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [".development.env"],
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      keepConnectionAlive: true,
+      synchronize: true,
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
