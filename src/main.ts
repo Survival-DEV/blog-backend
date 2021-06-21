cc488b4a7e10a15136caa31e18fcb57bda88da01
bayanseder <bayansider88@gmail.com>
2021-06-21 23:41:53 +0300

    setup swagger #7

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('LECTURA')
+    .setDescription('blog API')
+    .setVersion('1.0')
+    .addTag('blogs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
