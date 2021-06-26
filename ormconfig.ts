8a7f65642ab32911e874d912cacc7d88723c916f
Nujood <shawar.nujood@gmail.com>
2021-06-26 23:05:57 +0300

    Blog-9: update ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,3 @@
-import { join } from 'path';
 import { ConnectionOptions } from 'typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
@@ -12,18 +11,14 @@ const config = {
 const connectionOptions: TypeOrmModuleOptions | ConnectionOptions = {
   type: 'postgres',
   url: config.url,
-  entities: [join(__dirname, '**/**', '*.entity.{ts,js}')],
+  entities: ['dist/src/**/*.entity.js'],
   synchronize: true,
-  dropSchema: false,
-  keepConnectionAlive: true,
+  migrations: ['dist/src/database/migrations/*.js'],
   logging: ['warn', 'error'],
   logger: process.env.NODE_ENV === PROD_ENV ? 'file' : 'debug',
-  migrations: ['dist/database/migrations/*{.ts}'],
   cli: {
-    entitiesDir: 'dist/database/entities',
-    migrationsDir: 'dist/database/migrations',
+    migrationsDir: 'src/database/migrations',
   },
-  migrationsRun: true,
 };
 
 export default connectionOptions;
