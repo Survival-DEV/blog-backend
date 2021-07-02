0958b1b7b3434dee606726b9c69e781982b99e17
Nujood <shawar.nujood@gmail.com>
2021-07-02 22:51:58 +0300

    Blog-22: update typeorm connection options

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,30 +9,24 @@ import { CategoriesModule } from './categories/categories.module';
 import { BlogsModule } from './blogs/blogs.module';
 import { UsersModule } from './users/users.module';
 import { TagsModule } from './tags/tags.module';
-import { Tag } from "./tags/entities/tag.entity";
-import { BlogMetaModule } from './blog-meta/blog-meta.module';
-import { BlogMetaEntity } from './blog-meta/entities/blog-meta.entity';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-          isGlobal: true,
-          envFilePath: ['.env.development'],
-        }),
+      isGlobal: true,
+      envFilePath: ['.env.development'],
+    }),
     TypeOrmModule.forRoot({
-      type: "postgres",
+      type: 'postgres',
       url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      keepConnectionAlive: true,
-      entities: [UserEntity, BlogEntity,Tag, BlogMetaEntity],
+      entities: [__dirname + '/../**/*.entity.js'],
       synchronize: true,
     }),
     UsersModule,
     BlogsModule,
     CategoriesModule,
     TagsModule,
-    BlogMetaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
