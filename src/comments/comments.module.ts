76f90783616fb4c1b7f26a3395d30ea1f610bfe0
Nujood <shawar.nujood@gmail.com>
2021-07-22 21:29:29 +0300

    Blog-9: inject blogService into comments module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,10 +3,11 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { CommentEntity } from './entities/comment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogsModule } from 'src/blogs/blogs.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
   controllers: [CommentsController],
   providers: [CommentsService],
+  imports: [BlogsModule, TypeOrmModule.forFeature([CommentEntity])],
 })
 export class CommentsModule {}
