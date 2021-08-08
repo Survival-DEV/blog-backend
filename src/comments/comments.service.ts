99dc012f8ea01406a843391be2e6d785048be2d5
Nujood <shawar.nujood@gmail.com>
2021-08-08 16:30:20 +0300

    Chore: naming & remove comments

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -22,36 +22,19 @@ export class CommentsService {
     createCommentDto: CreateCommentDto,
   ): Promise<CommentEntity[] | CommentEntity | string | CommentEntity> {
     if (!createCommentDto.parent_id) {
-      const cat = await this.commentTreeRepository.save(createCommentDto);
-      return await this.commentTreeRepository.findAncestors(cat);
+      const parent = await this.commentTreeRepository.save(createCommentDto);
+      return await this.commentTreeRepository.findAncestors(parent);
     }
     const parent = await this.commentTreeRepository.findOne(
       createCommentDto.parent_id,
     );
-    
+
     const child = await this.commentTreeRepository.create({
       content: createCommentDto.content,
       blog_id: createCommentDto.blog_id,
     });
-    
-    // return await this.commentTreeRepository.createDescendantsQueryBuilder(
-    //   'comments',
-    //   'comments_closure',
-    //   parent,
-    //   ).getMany()
     child.parent = parent;
-    
-    // res.parent= createCommentDto.parent_id
-    // const res = this.commentTreeRepository
-    //   .update('comments_closure', CommentEntity)
-    //   .set({ id_ancestor:  createCommentDto.parent_id })
-    //   .where('"id_descendant" = :id', { id: createCommentDto.id })
-    //   .execute();
-    // const res = await this.commentTreeRepository.query(
-    //   `INSERT INTO comments_closure(id_ancestor,id_descendant)
-    //   VALUES ($1 , $2)`,
-    //   [createCommentDto.parent_id, createCommentDto.id],
-    //   );
+       
     return await this.commentTreeRepository.save(child);
   }
 
