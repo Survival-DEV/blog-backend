cde65ce4611fe0160b1d1c8410487bdd03ffed71
Nujood <shawar.nujood@gmail.com>
2021-08-10 15:02:42 +0300

    Chore: move main.ts to src dir instead of src/modules

--- src/tests/app.controller.spec.ts
+++ src/tests/app.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from '../modules/app.controller';
+import { AppController } from '../app.controller';
 import { AppService } from '../modules/app.service';
 
 describe('AppController', () => {
