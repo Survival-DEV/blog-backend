ad3982fd1b06cab8842723b6cf8d15f8ab72a46c
Nujood <shawar.nujood@gmail.com>
2021-08-11 16:38:21 +0300

    Blog-11: make meta_title optional at create-tag-dto

--- src/modules/tags/dto/create-tag.dto.ts
+++ src/modules/tags/dto/create-tag.dto.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTagDto {
-  @ApiProperty({type:String , description: 'tag id'})
-   id: string;
+  @ApiProperty({ type: String, description: 'tag id' })
+  id: string;
 
-  @ApiProperty({type:String , description: 'title'})
-   title: string;
-  
-  @ApiProperty({type:String , description: 'meta title'})
-   meta_title: string;
+  @ApiProperty({ type: String, description: 'title' })
+  title: string;
 
-  @ApiProperty({type:String , description: 'slug'})
+  @ApiProperty({ type: String, description: 'meta title' })
+  meta_title?: string;
+
+  @ApiProperty({ type: String, description: 'slug' })
   slug: string;
 }
