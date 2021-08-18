89e7e1e7444b02fd0c74db647f6bedee4ee9066b
Nujood <shawar.nujood@gmail.com>
2021-08-18 07:45:10 +0300

    Blog-31: Inject jwt & local authentication into our app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, UseGuards, Get, Post, Request } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { AuthService } from './modules/auth/auth.service';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './modules/auth/guards/local-auth.guard';
+import { LoginUserDto } from './modules/users/dto/login-user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
-  @Get()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  @ApiBody({ type: [LoginUserDto] })
+  login(@Request() req): any {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
