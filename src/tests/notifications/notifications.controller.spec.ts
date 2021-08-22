f7c50b8e4c2a8e80eff28116769e297e86f198c4
Nujood <shawar.nujood@gmail.com>
2021-08-23 00:03:26 +0300

    Blog #33: Add tests for notifications module

---
+++ src/tests/notifications/notifications.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { NotificationsController } from '../../modules/notifications/notifications.controller';
+import { NotificationsService } from '../../modules/notifications/notifications.service';
+
+describe('NotificationsController', () => {
+  let controller: NotificationsController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [NotificationsController],
+      providers: [NotificationsService],
+    }).compile();
+
+    controller = module.get<NotificationsController>(NotificationsController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
