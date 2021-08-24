a4a4ec92a65cd3dfd3e53eb5329892bed12749d8
Nujood <shawar.nujood@gmail.com>
2021-08-24 23:02:17 +0300

    Add new constant' Error messages

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, HttpStatus } from '@nestjs/common';
 import { ConflictException, HttpException } from '@nestjs/common/exceptions';
 import { InjectRepository } from '@nestjs/typeorm';
-import { InsertResult, Repository, UpdateResult } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import bcrypt from 'bcrypt';
 
 import { UserEntity } from '../../models/entities/user.entity';
@@ -9,6 +9,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginCredentialsPayload } from '../auth/interface/payload.interface';
 import { comparePasswords } from '../../utils';
 import { ERRORS } from '../../constants';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -25,7 +26,7 @@ export class UsersService {
     return this.usersRepository.findOne(id);
   }
 
-  //TODO: correct the input type name
+  //TODO: correct this input type
   async findByLogin({
     email,
     password,
@@ -38,6 +39,12 @@ export class UsersService {
     if (!user)
       throw new HttpException(ERRORS.USER_NOT_FOUND, HttpStatus.UNAUTHORIZED);
 
+    if (!user.isEmailConfirmed) {
+      throw new HttpException(
+        ERRORS.USER_EMAIL_NOT_VERIFIED,
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
     const areMatchedPasswords = comparePasswords(password, user.password);
 
     if (!areMatchedPasswords) {
@@ -49,16 +56,34 @@ export class UsersService {
     return user;
   }
 
+  async findByEmail(email: string): Promise<UserEntity> {
+    console.log('----------FindBy email', email);
+
+    return await this.usersRepository.findOneOrFail({ email });
+  }
+
+  async markEmailAsConfirmed(email: string) {
+    console.log('----------Marked as confiremd', email);
+
+    return this.usersRepository.update(
+      {
+        email,
+      },
+      { isEmailConfirmed: true },
+    );
+  }
+
   async update(data: UpdateUserDto, id: string): Promise<UpdateResult> {
     return await this.usersRepository.update({ id }, data);
   }
 
-  async create(data: any): Promise<InsertResult> {
+  async create(data: CreateUserDto): Promise<UserEntity> {
     try {
       if (!!data.password) {
         data.password = await bcrypt.hash(data.password, 12);
       }
-      return await this.usersRepository.insert(data);
+
+      return await this.usersRepository.create(data);
     } catch (error) {
       if (error.code === '23505') {
         throw new ConflictException(ERRORS.USER_EMAIL_ALREADY_EXISTS);
