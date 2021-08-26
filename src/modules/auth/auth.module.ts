29016a8a3611e0080523ac3913c3526cc202295b
Nujood <shawar.nujood@gmail.com>
2021-08-26 08:05:17 +0300

    Implement basic RBAC policy for authorization

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
 import { jwtConstants } from '../../constants';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { PermissionsGuard } from './guards/permissions.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
@@ -17,7 +19,15 @@ import { LocalStrategy } from './strategies/local.strategy';
       signOptions: { expiresIn: jwtConstants.expiresIn },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: PermissionsGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
