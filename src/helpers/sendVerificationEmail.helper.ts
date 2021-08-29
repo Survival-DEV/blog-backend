7c166bc8af5011aafa2ddf39dccbaa2bbf5ae91a
Nujood <shawar.nujood@gmail.com>
2021-08-29 14:08:12 +0300

    Update the crying sendGrid import

--- src/helpers/sendVerificationEmail.helper.ts
+++ src/helpers/sendVerificationEmail.helper.ts
@@ -1,7 +1,10 @@
+import { config } from 'dotenv';
 import { Client } from '@sendgrid/client';
-import SendGridService from '@sendgrid/mail';
+import { MailService } from '@sendgrid/mail';
 import { generateAuthToken } from './tokenGenerator.helper';
 
+config();
+const SendGridService = new MailService();
 SendGridService.setApiKey(process.env.SEND_GRID_ACCESS_KEY);
 SendGridService.setClient(new Client());
 SendGridService.setSubstitutionWrappers('{{', '}}');
