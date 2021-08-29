3ed6594e54307b5ebcfc2799c2cd2b31c23f64b3
Nujood <shawar.nujood@gmail.com>
2021-08-29 13:59:26 +0300

    Update token helper file

--- src/helpers/tokenGenerator.helper.ts
+++ src/helpers/tokenGenerator.helper.ts
@@ -3,12 +3,17 @@ import { JwtConstants } from 'src/constants';
 import { TokenParams } from 'src/core/auth/interface/login-status.interface';
 import { LoginCredentialsPayload } from 'src/core/auth/interface/payload.interface';
 
+const jwtService = new JwtService({
+  secret: JwtConstants.secret,
+  signOptions: { expiresIn: JwtConstants.expiresIn },
+});
+
 export const generateAuthToken = async ({
   email,
   password,
 }: LoginCredentialsPayload): Promise<TokenParams> => {
   return {
     expiresIn: JwtConstants.expiresIn,
-    accessToken: await JwtService.signAsync({ email, password }),
+    accessToken: await jwtService.signAsync({ email, password }),
   };
 };
